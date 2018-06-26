e0ae68e6ea5857fd127d0811d16f9fc555669e0c
Abdulhadi Hallak <hallak.aa@gmail.com>
2018-06-26 18:20:18 +0300

    add users relation to the permission model

--- src/Models/Permission.js
+++ src/Models/Permission.js
@@ -16,6 +16,10 @@ class Permission extends Model {
       description: 'min:3|max:1000'
     }
   }
+  
+  users () {
+    return this.belongsToMany('App/Models/User')
+  }
 }
 
 module.exports = Permission
